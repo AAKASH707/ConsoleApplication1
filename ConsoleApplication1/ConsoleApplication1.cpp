06070e0f06eff3f0cdcc40ef1452720a170f63c9
Lucifer <lucifer6218@gmail.com>
2020-02-25 23:15:13 +0530

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,113 @@
+/*#include<iostream>
+using namespace std;
+
+class cars
+{
+public:
+
+	string name;
+	string model;
+	string price;
+};
+
+int main()
+{
+	cars car1,car2;
+
+	car1.name = "BMW";
+	car1.model = "X500";
+	car1.price = "5M";
+
+
+	cout << "The name of car is: " << car1.name << endl;
+	cout << "The model of car is: " << car1.model << endl;
+	cout << "The price of the car is: " << car1.price << endl;
+
+	return 0;
+
+}   */
+
+/*#include<iostream>
+using namespace std;
+
+class student
+{
+public:
+	string name;
+	int  SAP_ID;
+	int roll_no;
+
+};
+
+int main()
+{
+	student student1;
+
+	student1.name = "aakash";
+	student1.SAP_ID = 500075995;
+	student1.roll_no = 001;
+
+	cout << "Name of the student is :" << student1.name << endl;
+	cout << "SAP ID of the student :" << student1.SAP_ID << endl;
+	cout << "ROLL no: " << student1.roll_no << endl;
+
+	return 0;
+
+}
+*/
+
+/* #include<iostream>
+using namespace std;
+
+class first
+{
+public:
+
+	void function();
+
+};
+
+ void first::function()
+{
+	cout << "Hello World!!" << endl;
+}
+
+
+int main()
+{
+	first print1;
+	print1.function();
+	return 0;
+
+
+		
+ }
+ */
+
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "I am Lucifer: ";
+	return 0;
+}*/
+
+
+
+#inclde<iostream>
+using namespace std;
+ int main()
+{	
+	int n;
+	cout<<"print your favourite number b/w 1 to 100: "<<endl;
+	cin>>n;
+	cout<< "That my favourite number too<< n<<endl;
+	return 0;
+}
+ 
+
+
+
+
