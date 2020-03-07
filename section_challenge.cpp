3b7496744c3b987f6cbe6f5046380e4f7008a0f7
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-08 01:15:41 +0530

    Create section_challenge.cpp

---
+++ section_challenge.cpp
@@ -0,0 +1,11 @@
+// Print favourite number from user //
+#inclde<iostream>
+using namespace std;
+ int main()
+{	
+	int n;
+	cout<<"print your favourite number b/w 1 to 100: "<<endl;
+	cin>>n;
+	cout<< "That my favourite number too<< n<<endl;
+	return 0;
+}
