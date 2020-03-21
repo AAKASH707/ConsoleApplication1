b406d7f428d389407dd23a9693734ee1d7074c8a
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-21 20:24:03 +0530

    SORTING PROGRAM PART-2

---
+++ sorting programs.c
@@ -0,0 +1,90 @@
+*****************************************************************************************************************************************
+                                                      Selection Sort Program
+*****************************************************************************************************************************************
+#include<stdio.h>
+int main(){
+   /* Here i & j for loop counters, temp for swapping,
+    * count for total number of elements, number[] to
+    * store the input numbers in array. You can increase
+    * or decrease the size of number array as per requirement
+    */
+   int i, j, count, temp, number[25];
+
+   printf("How many numbers u are going to enter?: ");
+   scanf("%d",&count);
+
+   printf("Enter %d elements: ", count);
+   // Loop to get the elements stored in array
+   for(i=0;i<count;i++)
+      scanf("%d",&number[i]);
+ 
+   // Logic of selection sort algorithm
+   for(i=0;i<count;i++){
+      for(j=i+1;j<count;j++){
+         if(number[i]>number[j]){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+   }
+
+   printf("Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
+
+   return 0;
+}
+****************************************************************************************************************************************
+****************************************************************************************************************************************
+                                                           Quicksort program
+****************************************************************************************************************************************
+****************************************************************************************************************************************
+#include<stdio.h>
+void quicksort(int number[25],int first,int last){
+   int i, j, pivot, temp;
+
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+            i++;
+         while(number[j]>number[pivot])
+            j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      quicksort(number,first,j-1);
+      quicksort(number,j+1,last);
+
+   }
+}
+
+int main(){
+   int i, count, number[25];
+
+   printf("How many elements are u going to enter?: ");
+   scanf("%d",&count);
+
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+      scanf("%d",&number[i]);
+
+   quicksort(number,0,count-1);
+
+   printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
+
+   return 0;
+}
